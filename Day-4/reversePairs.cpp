be420f22fd4e4fec3d8659cc77986d272c796b8f
unknown <vaibhavnangia2001@gmail.com>
2022-06-13 17:24:54 +0530

    day 4 challenge completed

---
+++ Day-4/reversePairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reversePairs(vector<int>& nums) {
+        int reversePairCount = 0;
+for(int i=0; i<nums.size(); i++) {
+	for(int j=i+1; j<nums.size(); j++) {
+		if( nums[i] > 2*nums[j]) reversePairCount++;
+	}
+}
+return reversePairCount;
+    }
+};
