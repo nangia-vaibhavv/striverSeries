bdacdaab84b2917b9f0b52ac187f56229ce679a3
unknown <vaibhavnangia2001@gmail.com>
2022-06-16 12:38:18 +0530

    day 7 challenge completed

---
+++ Day-7/removeNnodeFromBack.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode*s=head;
+        ListNode*e=head;
+      
+        for(int i=0;i<n;i++)
+        {
+            e=e->next;
+        }
+       if(e==NULL) return head->next;
+        while(e->next)
+        {
+            e=e->next;
+            s=s->next;
+        }
+        s->next=s->next->next;
+        return head;
+    }
+   
+
+
+
+};
