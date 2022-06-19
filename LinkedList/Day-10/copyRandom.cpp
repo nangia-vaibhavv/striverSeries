38a6d0f83524989d64442cb642d9d82ffb3f5184
unknown <vaibhavnangia2001@gmail.com>
2022-06-19 17:38:21 +0530

    day 10 challenge completed

---
+++ LinkedList/Day-10/copyRandom.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    private:
+    unordered_map<Node*, Node *>m;
+public:
+    Node* copyRandomList(Node* head) {
+        if(!head) return NULL;
+        if(m[head]) return m[head];
+        m[head]=new Node(head->val);
+        m[head]->next=copyRandomList(head->next);
+        m[head]->random=copyRandomList(head->random);
+        return m[head];
+    }
+};
