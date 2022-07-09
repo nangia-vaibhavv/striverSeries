85ae4359451faf0386694431438b11546125a8fa
unknown <vaibhavnangia2001@gmail.com>
2022-07-09 13:36:42 +0530

    bst ques

---
+++ BinarySearchTree/arrayToBST.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    TreeNode *solve(vector<int>&nums, int start, int end)
+    {
+        if(end<=start) return NULL;
+        int mid = (start + end)/2;        
+        TreeNode*root=new TreeNode(nums[mid]);
+        root->left=solve(nums,start,mid);
+        root->right=solve(nums,mid+1,end);
+        return root;
+    }
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+       return solve(nums,0,nums.size());
+    }
+};
