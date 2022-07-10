bf12ed72e46792650d4acc60a13c644306e50804
unknown <vaibhavnangia2001@gmail.com>
2022-07-10 15:14:43 +0530

    bst day 2 completed

--- BinarySearchTree2/twoSumInBSTExists.cpp
+++ BinarySearchTree2/twoSumInBSTExists.cpp
@@ -29,3 +29,19 @@ public:
        return findTwoSum(res,k);
     }
 };
+
+// or
+class Solution {
+    unordered_set<int> s;
+public:
+    bool findTarget(TreeNode* root, int k) {
+        if(!root)
+            return false;
+
+        if(s.count(k - root->val))
+            return true;
+        else
+            s.insert(root->val);
+        return findTarget(root->left, k) || findTarget(root->right, k);
+    }
+};
