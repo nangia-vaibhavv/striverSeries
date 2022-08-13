8d056186093ccd97ef9b6c9d8c92fa9e37e254a6
unknown <vaibhavnangia2001@gmail.com>
2022-08-13 12:52:27 +0530

    subsequencce

--- DP/dpOnString/longestCommonSubstring.cpp
+++ DP/dpOnString/longestCommonSubstring.cpp
@@ -5,7 +5,7 @@ int lcs(string &s, string &t){
     vector<vector<int>>dp(n+1,vector<int>(m+1,0));
     for(int j=0;j<=m;j++)dp[0][j]=0;
     for(int i=0;i<=n;i++)dp[i][0]=0;
-    int ans=0;
+    int res=0;
     for(int i=1;i<=n;i++)
     {
         for(int j=1;j<=m;j++)
@@ -13,10 +13,10 @@ int lcs(string &s, string &t){
             if(s[i-1]==t[j-1])
             {
                 dp[i][j]=1+dp[i-1][j-1];
-                ans=max(ans,dp[i][j]);
+                res=max(res,dp[i][j]);
             }
             else dp[i][j]=0;
         }
     }
-    return ans;
+    return res;
 }
